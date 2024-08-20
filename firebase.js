700c53c43562f054dc78e0070255f241089b6f4c
Alvin Wong <alvinwong@ufl.edu>
2024-08-20 00:46:05 -0400

    Changes

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
+require('dotenv').config();
+
 import { initializeApp } from "firebase/app";
-import {getFirestore} from "firebase/firestore"
+import {getFirestore} from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
